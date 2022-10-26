c7feb1555a8f447d46ab017c260e2cddf5c61e44
LitaAmel <107165443+LitaAmel@users.noreply.github.com>
2022-10-26 13:49:14 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php 
+    include "header.php";
+?>
+
+<center>
+    <style>
+        body
+        {background-image: url(usaha-1.jpg);}
+      h2{color:red;
+    margin-top: 200px;
+}
+        h2{
+            color:black;
+            margin-top: 300px;
+        }
+    </style>
+
+    <h2> <span style="color:white;
+background-color:darkgreen;
+padding:0 40px;
+border-radius:9px;">
+ SEMANGAT SEMANGAT OKE OKE  </span></h2>
+   
+</center>
---
+++ produk.php
@@ -0,0 +1,58 @@
+<?php
+include "header.php"; 
+?>
+<head>
+    <link rel="stylesheet" href="edit.css">
+</head>
+<div class="container rounded" style="margin-top:10px">
+<h2 align="center" style="margin:30px 0 35px"> Produk</h2>
+    <form method="post"style="margin:30px 30px 10px" action="produk.php" class="d-flex">
+        <input class="form-control" type="search" name="cari" placeholder="Search" aria-label="Search">
+        <button class="btn  "style="background-color:#251B37; color:white" type ="submit">Search</button>
+    </form>
+    
+<div class="row">
+        <?php 
+            include "koneksi.php";
+            if(isset($_POST['cari'])){
+                $cari = $_POST['cari'];
+                $qry_produk=mysqli_query($conn, "select * from produk where id_produk = '$cari' or nama_produk like '%$cari%' or deskripsi like '%$cari%'");
+            }
+            else{
+                $qry_produk = mysqli_query($conn, "select * from produk");
+            }
+            $no = 0;
+            while ($dt_produk = mysqli_fetch_array($qry_produk)) {
+                $no++;
+        ?>
+            <div class="col-md-3">
+                <div class="card mt-3" style="background-color:#94B49F; border-radius:30px" >
+                    <img src="../fotonya/<?=$dt_produk['foto_produk']?>" class="card-img-top" width="180px" height="260px" style="border-radius:30px 30px 0 0">
+                    <div class="card-body">
+                        <h5 class="card-title"><?=$dt_produk['nama_produk']?></h5>
+                        <p class="card-text"><?=substr($dt_produk['deskripsi'], 0,30)?></p>
+                        <p class="card-text"><?=substr($dt_produk['harga'], 0,30)?></p>
+
+                        <a href="ubah_produk.php?id_produk=<?=$dt_produk['id_produk']?>" class="btn" style="background-color:#ECB390; color:black">Ubah<a>
+                        <a href="hapus_produk.php?id_produk=<?=$dt_produk['id_produk']?>" onclick="return confirm('Apakah anda yakin menghapus data ini?')" class="btn btn-danger">Hapus</a>
+
+
+                        
+
+
+                    </div>
+                </div>  
+            </div>
+        <?php
+        }
+        ?>
+</div>
+</div>
+
+    <div>
+        <br>
+        <a href="tambah_produk.php" class="btn" style="background-color:#ECB390;color:black">Tambah Produk</a>
+    </div>
+<?php
+include "footer.php";
+?>
